4a34fd586fd92577e5b4e4e255b009bbbbcef0b3
AyoubBoulaid <boulaidayoub02@gmail.com>
2024-06-28 09:21:55 -0700

    COnfiguration Management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,23 +1,3 @@
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command   => '/usr/bin/pip3 install Flask==2.1.0',
-  path      => ['/bin', '/usr/bin'],
-  unless    => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  logoutput => true,
-}
-
-file { '/etc/profile.d/flask_path.sh':
-  ensure  => 'file',
-  content => 'export PATH=$PATH:/usr/local/bin',
-  mode    => '0755',
-}
-
-exec { 'source_profile':
-  command   => 'source /etc/profile',
-  path      => ['/bin', '/usr/bin'],
-  require   => File['/etc/profile.d/flask_path.sh'],
-  logoutput => true,
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
